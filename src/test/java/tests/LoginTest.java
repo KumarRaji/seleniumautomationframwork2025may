588752894b60f02ddeaeb12a0e5bc6e7876834d1
kumar <kumar.rit8@gmail.com>
2025-05-18 10:11:30 +0530

    first commit

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+
+	@Test
+	public void testValidLogin() {
+		LoginPage loginpage = new LoginPage(driver);
+		loginpage.enterUsername("admin@yourstore.com");
+		loginpage.enterPassword("admin");
+		loginpage.clickLogin();
+		
+		System.out.println("Title of the page is : " +driver.getTitle());
+	}
+}
